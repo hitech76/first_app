cfff258648e8ff6ea4d4f7de13ce2a07e2bc7508
U-STABILIX\Jayson <Jayson@STXLAPTOP25.stabilixindia.biz>
2011-12-14 10:59:13 +0530

    first version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e508064dd2abbf979b90fa59e39c2213fa94cea502e0607340890bdfc704768dbe68008ae072484be30887eb5387957dc186eb80729604e03ababd2c4e808630'
